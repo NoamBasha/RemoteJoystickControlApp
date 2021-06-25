005cccc38c03205bf8caae5e1f6cfe521604cca4
NoamBasha <noamba10@gmail.com>
2021-06-25 12:58:20 +0300

    ViewModel v1 - not sure.

--- app/src/main/java/com/example/remotejoystickcontrolapp/view_model/ViewModel.java
+++ app/src/main/java/com/example/remotejoystickcontrolapp/view_model/ViewModel.java
@@ -1,4 +1,48 @@
 package com.example.remotejoystickcontrolapp.view_model;
 
+import com.example.remotejoystickcontrolapp.model.FGPlayer;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ViewModel {
+
+    private FGPlayer fgPlayer;
+    public String ip;
+    public int port;
+
+    public ViewModel(FGPlayer fgPlayer) {
+        this.fgPlayer = fgPlayer;
+    }
+
+    // Establishing connection and running a thread for the "tasks"
+    public void connect() throws Exception {
+        this.fgPlayer.connect(ip,port);
+    }
+
+    // Disconnecting and setting isConnected to false
+    public void disconnect() throws Exception {
+        this.fgPlayer.disconnect();
+    }
+
+    // Aileron task
+    public void sendAileron(float v) throws InterruptedException {
+        //TODO Should normalize v?
+        this.fgPlayer.sendAileron(v);
+    }
+
+    // Elevator task
+    public void sendElevator(float v) throws InterruptedException {
+        this.fgPlayer.sendElevator(v);
+    }
+
+    // Rudder task
+    public void sendRudder(float v) throws InterruptedException {
+        this.fgPlayer.sendRudder(v);
+    }
+
+    // Throttle task
+    public void sendThrottle(float v) throws InterruptedException {
+        this.fgPlayer.sendThrottle(v);
+    }
 }
--- app/src/main/java/com/example/remotejoystickcontrolapp/views/MainActivity.java
+++ app/src/main/java/com/example/remotejoystickcontrolapp/views/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.remotejoystickcontrolapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.remotejoystickcontrolapp.R;
+import com.example.remotejoystickcontrolapp.databinding.ActivityMainBinding;
+import com.example.remotejoystickcontrolapp.model.FGPlayer;
+import com.example.remotejoystickcontrolapp.view_model.ViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Fields
+    private FGPlayer fgPlayer;
+    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        this.fgPlayer = new FGPlayer();
+        this.viewModel = new ViewModel(fgPlayer);
+        activityMainBinding.setViewModel(viewModel);
+
     }
 }
